20758a1e68d25a01c993de5b4b37a184da897e06
Val <valentin.stashevski@gmail.com>
2025-01-06 19:03:45 +0200

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -17,7 +17,7 @@ def test_homepage():
 
 def test_get_available_streams(video_url):
     """Test the video details fetch endpoint."""
-    response = client.get("/available_streams?video_url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DKPtTzhS0TEs")
+    response = client.get(f"/available_streams?video_url={video_url}")
     assert response.status_code == 200
     data = response.json()
     assert "thumbnail_url" in data
